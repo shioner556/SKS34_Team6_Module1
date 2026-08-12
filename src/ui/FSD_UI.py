cb6f3a50e744119136cb4621c071a0b49b4f373c
xorud989 <xorud989@gmail.com>
2026-08-12 11:12:22 +0900

    파일 다수적용 가능

--- src/ui/FSD_UI.py
+++ src/ui/FSD_UI.py
@@ -10,7 +10,8 @@ from preprocessor import preprocess
 st.title('FSD(File-Sniffing-Dog)')
 st.write('---')
 # 파일 업로드 부분
-uploaded_file = st.file_uploader('🐶제가 냄새를 맡을 수 있게 파일을 업로드 해주세요!')
+uploaded_files = st.file_uploader('🐶제가 냄새를 맡을 수 있게 파일을 업로드 해주세요!', accept_multiple_files = True) 
+sniff_clicked = st.button('냄새 맡기')
 st.write('---')
 col1, mid, col2 = st.columns([10, 1, 10])
 
@@ -18,14 +19,14 @@ with col1:
     st.subheader('1단계 결과')
     # 파일 탐지중 메세지 구분
     #---------------------------------------
-    if uploaded_file is not None:
+    if uploaded_files and sniff_clicked :
         placeholder = st.empty()
         dots = ["", ".", "..", "..."]
         end_time = time.time() + random.uniform(0,5)
 
         i = 0
         while time.time() < end_time :
-            placeholder.markdown(f"🐶 가방 겉면 킁킁대는 중...{dots[i % 4]}")
+            placeholder.markdown(f"🐶 가방 겉면 킁킁하는 중...{dots[i % 4]}")
             time.sleep(0.4)
             i += 1
 
@@ -33,11 +34,13 @@ with col1:
         #----------------------------------------
         # 1단계 (preproccsor.py) 연결코드
         with tempfile.TemporaryDirectory() as tmp_dir:
-            tmp_path = Path(tmp_dir) / uploaded_file.name
-            tmp_path.write_bytes(uploaded_file.getvalue())
-            result = preprocess(tmp_path)
-        # (preproccsor.py) 결과 출력 
-            st.json(result)
+            for uploaded_file in uploaded_files:
+                tmp_path = Path(tmp_dir) / uploaded_file.name
+                tmp_path.write_bytes(uploaded_file.getvalue())
+                result = preprocess(tmp_path)
+        # 출력코
+                with st.expander(f"📄 {uploaded_file.name}"):
+                    st.json(result)
 
 with mid:
     st.markdown(
