1c17a142df89f058881b9cf357f9cc7cc67e6b85
shioner556 <shioner556@gmail.com>
2026-08-14 15:36:49 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,18 +3,18 @@ from pathlib import Path
 from streamlit.web import cli as stcli
 
 def main():
-    # main.py가 src/ 안에 있으므로 current_dir은 'src' 폴더입니다.
-    current_dir = Path(__file__).resolve().parent
+    # root 디렉토리 지정.
+    root_dir = Path(__file__).resolve().parent
     
-    # 'src' 폴더 바로 아래의 'ui/FSD_UI.py'를 지정합니다.
-    ui_script_path = current_dir / "ui" / "FSD_UI.py"
+    # 'src' 폴더 바로 아래의 'ui/FSD_UI.py' 지정.
+    ui_script_path = root_dir / "ui" / "FSD_UI.py"
     
     if not ui_script_path.exists():
         print(f"[ERROR] UI 파일을 찾을 수 없습니다: {ui_script_path}")
         return
 
     print("=== AI 파일 업로드 악성코드 탐지 시스템 (Streamlit UI) 구동 중... ===")
-    
+
     sys.argv = ["streamlit", "run", str(ui_script_path)]
     sys.exit(stcli.main())
 
